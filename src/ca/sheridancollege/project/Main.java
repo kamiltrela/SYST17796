b72d13b96a485636cff30e51798b3bc0d6ca396d
estefaniamelo <melojoa@sheridancollege.ca>
2020-07-31 17:25:37 -0400

    distributeHand method in DeckOfCards class works, it generates and shuffles a new hand of cards for the players, it also takes off whatever cards were chosen out of the deckOfCards ArrayList.  Tested in the main method. Everything compiles.

--- src/ca/sheridancollege/project/Main.java
+++ src/ca/sheridancollege/project/Main.java
@@ -1,13 +1,24 @@
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 public class Main {
-    
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         DeckOfCards deck = new DeckOfCards();
+
         deck.generateDeck();
         //System.out.println(deck.deckOfCardsList);
         deck.shuffle(deck.deckOfCardsList);
         System.out.println(deck.deckOfCardsList);
+
+        System.out.println(deck.deckOfCardsList.size());
+        ArrayList tempHand = new ArrayList();
+
+        tempHand = deck.distributeHand();
+        System.out.println(tempHand);
+        System.out.println(deck.deckOfCardsList.size());
+
     }
 }
