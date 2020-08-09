9bd563994ba78c49611d458981ff883f418b7664
kamiltrela <trelaka@sheridancollege.ca>
2020-08-09 14:46:10 -0400

    UserInterface class: Added statement to call and run a game of go fish. Main class: Added code calling the UserInterface class which handles a game of go fish from start to finish.

--- src/ca/sheridancollege/project/Main.java
+++ src/ca/sheridancollege/project/Main.java
@@ -1,11 +1,17 @@
 package ca.sheridancollege.project;
 
+/**
+ * This is the Main class where we call the UserInterface class.
+ * The main method only calls one method from the UserInterface class,
+ * which handles the Go Fish game from start to finish.
+ * 
+ * @author Kamil Trela, 2020
+ */
 public class Main {
 
     public static void main(String[] args) {
-           GoFish game = new GoFish("Go Fish");
-           game.setUpGame();
-           game.play();
+        UserInterface ui = new UserInterface();
+        ui.playGoFish();
 
     }
 }
--- src/ca/sheridancollege/project/UserInterface.java
+++ src/ca/sheridancollege/project/UserInterface.java
@@ -3,16 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ca.sheridancollege.project;
 
 /**
- * This class will function as the "view". Its going to take care of user inputs
- * and outputs.
+ * This class creates a new instance of GoFish,
+ * it also starts the game by calling the setUpGame() method
+ * and calls the play() method which cycles through the game until someone wins.
  *
  * @author Kamil Trela, 2020
  * @author Estefania Melo, 2020
  */
 public class UserInterface {
+    
+    public UserInterface(){
+        
+    }
 
+    public void playGoFish() {
+        GoFish game = new GoFish("Go Fish");
+        game.setUpGame();
+        game.play();
+    }
 }
